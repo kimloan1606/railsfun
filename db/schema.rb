6acaf5d861757cd5a2f4105ebccf09e434e2d2f3
acv-binhvh <binhvh@atmarkcafe.org>
2018-01-10 15:34:43 +0700

    remove-column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110045217) do
+ActiveRecord::Schema.define(version: 20180110082639) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
