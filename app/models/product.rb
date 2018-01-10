08f60026b35fbe908ca2299306839ce773bcc287
acv-binhvh <binhvh@atmarkcafe.org>
2018-01-10 17:47:17 +0700

    add validate price, title , description

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ActiveRecord::Base
+ validates :title, :description, presence: true
+ validate :title_is_shorter_than_description
+ validates :price, presence: true, numericality: { greater_than: 0 }
+
+ def title_is_shorter_than_description
+ 	return if title.blank? or description.blank?
+ 	if description.length < title.length
+ 		errors.add(:description, 'can not be shorter han title')
+ 	end
+ end
+end
