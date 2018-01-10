bafebe2d80610f92db99967c15193a0a70293ea5
acv-binhvh <binhvh@atmarkcafe.org>
2018-01-10 16:01:56 +0700

    rollback to 6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110084226) do
+ActiveRecord::Schema.define(version: 20180110085325) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20180110084226) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "price", precision: 6, scale: 2
+    t.decimal "price", precision: 10, scale: 2
     t.boolean "published"
     t.string "short_description"
     t.integer "category_id"
