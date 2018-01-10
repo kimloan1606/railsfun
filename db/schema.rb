29782e070ec7d53272b6f90258a8e98250023756
acv-binhvh <binhvh@atmarkcafe.org>
2018-01-10 15:40:59 +0700

    add category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110082639) do
+ActiveRecord::Schema.define(version: 20180110083651) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
