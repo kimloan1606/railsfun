f84f20a4477ccb1b3cd74e7fd753d205cb3f5ae4
acv-binhvh <binhvh@atmarkcafe.org>
2018-01-11 16:14:18 +0700

    title lower case

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,16 @@ class Product < ActiveRecord::Base
  validates :title, :description, presence: true
  validate :title_is_shorter_than_description
  validates :price, presence: true, numericality: { greater_than: 0 }
+ before_save :strip_html_from_description
+ before_validation :title_lower_case
+ def title_lower_case
+ 	self.title = title.downcase
+ 	
+ end
+
+ def strip_html_from_description
+ 	self.description= ActionView::Base.full_sanitizer.sanitize(self.description)
+end
 
  def title_is_shorter_than_description
  	return if title.blank? or description.blank?
