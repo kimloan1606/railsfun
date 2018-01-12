65e61f2d728cd90bdd7489fa3f285282e483ecb8
acv-binhvh <binhvh@atmarkcafe.org>
2018-01-12 17:36:25 +0700

    edit product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,22 +5,35 @@ class ProductsController < ApplicationController
 	def show
 		@product = Product.find(params[:id])
 	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+		flash[:notice] = 'thanh cong'
+		return redirect_to products_path if @product.save
+		flash.now[:notice] ='ko thanh cong'
+		render :new
+	end
+	def edit
+		@product = Product.find(params[:id])
+
+		render :new
+	end
+	def update
+		@product =Product.find(params[:id])
+		if @product.update(product_params)
+					return redirect_to products_path
+				else flash.now[:notice] ='ko thanh cong'
+					render :new
+		end
 		
-		def new
-			@product = Product.new
-			
-		end
-		def create
-			@product = Product.new(product_params)
-			flash[:notice] = 'thanh cong'
-			return redirect_to products_path if @product.save
-			flash.now[:notice] ='ko thanh cong'
-			render :new
-			
-		end
+	end
 	private
 	def product_params
-		params.require(:product).permit(:title, :description, :price, :published, :category_id)
+		params.require(:product).permit(:title, :description, :price, :published, :category_id, :level)
 		
 	end
-	end
+end
